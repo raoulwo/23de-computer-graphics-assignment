a715eb1b9aa1de70338ac32f05c4375ab382b43f
Raoul Wograndl <wograndl.raoul@gmail.com>
2026-09-23 14:56:30 +0200

    feat: define normals

--- Assets/Scripts/ProceduralBlock.cs
+++ Assets/Scripts/ProceduralBlock.cs
@@ -27,40 +27,40 @@ public class ProceduralBlock : MonoBehaviour
         var vertices = new Vector3[]
         {
             // Front face vertices
-            new Vector3(-0.5f, -0.5f, -0.5f), // V0
-            new Vector3(0.5f, -0.5f, -0.5f), // V1
-            new Vector3(0.5f, 0.5f, -0.5f), // V2
-            new Vector3(-0.5f, 0.5f, -0.5f), // V3
+            new Vector3(-0.5f, -0.5f, -0.5f), // I0 (V0)
+            new Vector3(0.5f, -0.5f, -0.5f), // I1 (V1)
+            new Vector3(0.5f, 0.5f, -0.5f), // I2 (V2)
+            new Vector3(-0.5f, 0.5f, -0.5f), // I3 (V3)
 
             // Back face vertices
-            new Vector3(-0.5f, -0.5f, 0.5f), // V4
-            new Vector3(0.5f, -0.5f, 0.5f), // V5
-            new Vector3(0.5f, 0.5f, 0.5f), // V6
-            new Vector3(-0.5f, 0.5f, 0.5f), // V7
+            new Vector3(-0.5f, -0.5f, 0.5f), // I4 (V4)
+            new Vector3(0.5f, -0.5f, 0.5f), // I5 (V5)
+            new Vector3(0.5f, 0.5f, 0.5f), // I6 (V6)
+            new Vector3(-0.5f, 0.5f, 0.5f), // I7 (V7)
 
             // Right face vertices
-            new Vector3(0.5f, -0.5f, -0.5f), // V1
-            new Vector3(0.5f, -0.5f, 0.5f), // V5
-            new Vector3(0.5f, 0.5f, 0.5f), // V6
-            new Vector3(0.5f, 0.5f, -0.5f), // V2
-            
+            new Vector3(0.5f, -0.5f, -0.5f), // I8 (V1)
+            new Vector3(0.5f, -0.5f, 0.5f), // I9 (V5)
+            new Vector3(0.5f, 0.5f, 0.5f), // I10 (V6)
+            new Vector3(0.5f, 0.5f, -0.5f), // I11 (V2)
+
             // Left face vertices
-            new Vector3(-0.5f, -0.5f, 0.5f), // V4
-            new Vector3(-0.5f, -0.5f, -0.5f), // V0
-            new Vector3(-0.5f, 0.5f, -0.5f), // V3
-            new Vector3(-0.5f, 0.5f, 0.5f), // V7
-            
+            new Vector3(-0.5f, -0.5f, 0.5f), // I12 (V4)
+            new Vector3(-0.5f, -0.5f, -0.5f), // I13 (V0)
+            new Vector3(-0.5f, 0.5f, -0.5f), // I14 (V3)
+            new Vector3(-0.5f, 0.5f, 0.5f), // I15 (V7)
+
             // Top face vertices
-            new Vector3(-0.5f, 0.5f, -0.5f), // V3
-            new Vector3(0.5f, 0.5f, -0.5f), // V2
-            new Vector3(0.5f, 0.5f, 0.5f), // V6
-            new Vector3(-0.5f, 0.5f, 0.5f), // V7
-            
+            new Vector3(-0.5f, 0.5f, -0.5f), // I16 (V3)
+            new Vector3(0.5f, 0.5f, -0.5f), // I17 (V2)
+            new Vector3(0.5f, 0.5f, 0.5f), // I18 (V6)
+            new Vector3(-0.5f, 0.5f, 0.5f), // I19 (V7)
+
             // Bottom face vertices
-            new Vector3(-0.5f, -0.5f, 0.5f), // V4
-            new Vector3(0.5f, -0.5f, 0.5f), // V5
-            new Vector3(0.5f, -0.5f, -0.5f), // V1
-            new Vector3(-0.5f, -0.5f, -0.5f), // V0
+            new Vector3(-0.5f, -0.5f, 0.5f), // I20 (V4)
+            new Vector3(0.5f, -0.5f, 0.5f), // I21 (V5)
+            new Vector3(0.5f, -0.5f, -0.5f), // I22 (V1)
+            new Vector3(-0.5f, -0.5f, -0.5f), // I23 (V0)
         };
 
         // NOTE: Winding order in Unity is **clock-wise**.
@@ -73,16 +73,37 @@ public class ProceduralBlock : MonoBehaviour
             4, 5, 6, 4, 6, 7,
 
             // Right face triangles
-            1, 6, 5, 1, 2, 6,
-            
+            8, 10, 9, 8, 11, 10,
+
             // Left face triangles
-            4, 3, 0, 4, 7, 3,
-            
+            12, 14, 13, 12, 15, 14,
+
             // Top face triangles
-            3, 6, 2, 3, 7, 6,
-            
+            16, 18, 17, 16, 19, 18,
+
             // Bottom face triangles
-            4, 1, 5, 4, 0, 1,
+            20, 22, 21, 20, 23, 22
+        };
+
+        var normals = new Vector3[]
+        {
+            // Front face normals
+            Vector3.back, Vector3.back, Vector3.back, Vector3.back,
+
+            // Back face normals
+            Vector3.forward, Vector3.forward, Vector3.forward, Vector3.forward,
+
+            // Right face normals
+            Vector3.right, Vector3.right, Vector3.right, Vector3.right,
+
+            // Left face normals
+            Vector3.left, Vector3.left, Vector3.left, Vector3.left,
+
+            // Top face normals
+            Vector3.up, Vector3.up, Vector3.up, Vector3.up,
+
+            // Bottom face normals
+            Vector3.down, Vector3.down, Vector3.down, Vector3.down,
         };
 
         _mesh = new Mesh
@@ -90,6 +111,7 @@ public class ProceduralBlock : MonoBehaviour
             name = "ProceduralBlock",
             vertices = vertices,
             triangles = triangles,
+            normals = normals,
         };
 
         _meshFilter.sharedMesh = _mesh;
